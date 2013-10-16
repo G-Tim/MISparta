f9b5121128c3e967f4c6c6871ce8dbf4fd716b4d
GTim <GTim@MC-G-LPCc120-5.local>
2013-10-16 16:20:41 +0200

    add to repo

---
+++ Gladiateur/Gladiateur/Team.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Gladiateur
+{
+	public class Team
+	{
+		private int id, playedMatchs, wonMatchs, lostMatchs;
+		private double ratioWin;
+		private string userAlias, name, description;
+
+		public Team ()
+		{
+		}
+	}
+}
+
---
+++ Gladiateur/Gladiateur/User.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Gladiateur
+{
+	public class User
+	{
+		private string firstname, lastname, alias;
+		private DateTime registerDate;
+		private int Score;
+
+		public User ()
+		{
+		}
+	}
+}
+
