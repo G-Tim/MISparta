25c9aa3c9058eba83aa68b4434af6cc44a670ac2
BionikDelay <paulguzda@free.fr>
2013-10-16 15:59:03 +0200

    2. Add Equipment
    Ajout des classes d'objet de type "Equipment"

---
+++ Gladiateur/Gladiateur/Equipment/EquipmentBoth.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Gladiateur/Equipment
+{
+	public class EquipmentBoth : EquipmentAttack, EquipmentDefense
+	{
+		public EquipmentBoth ()
+		{
+		}
+	}
+}
+
---
+++ Gladiateur/Gladiateur/Equipment/EquipmentDefense.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Gladiateur/Equipment
+{
+	public class EquipmentDefense : Equipment
+	{
+		public EquipmentDefense ()
+		{
+		}
+	}
+}
+
