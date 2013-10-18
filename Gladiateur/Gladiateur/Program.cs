57ae33780fb4a6ba3aefef3e5d393b2a237af51d
BionikDelay <paulguzda@free.fr>
2013-10-18 14:45:32 +0200

    ...

--- Gladiateur/Gladiateur/Program.cs
+++ Gladiateur/Gladiateur/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 namespace Gladiateur
 {
 	class MainClass
+	{
+		public static void Main (string[] args)
 		{
 			//User Test = new User ("Tim", "G.", "Team Warior-Boss-JteDémonte");
 			//Console.WriteLine (Test.SePresenter()); 
@@ -14,33 +16,33 @@ namespace Gladiateur
 			/***** Initialisation des joueurs *****/
 			/**************************************/
 			List<User> LstUsers = new List<User> ();
-			User joueur1 = new User("Tim", "G.", "G-Tim-le-Magnifique");                        LstUsers.Add(joueur1);
-			User joueur2 = new User("Polo", "G.", "Polo-Seigneur-des-Enfers");                  LstUsers.Add(joueur2);
-			User joueur3 = new User("Nico", "M.", "Nicolas-Ambassadeur-des-Contrées-du-Nord");  LstUsers.Add(joueur3);
-			User joueur4 = new User("Guigui", "F.", "Guillaume-le-Grand");                      LstUsers.Add(joueur4);
+			User joueur1 = new User ("Tim", "G.", "G-Tim-le-Magnifique");                       LstUsers.Add (joueur1);
+			User joueur2 = new User ("Polo", "G.", "Polo-Seigneur-des-Enfers");                 LstUsers.Add (joueur2);
+			User joueur3 = new User ("Nico", "M.", "Nicolas-Ambassadeur-des-Contrées-du-Nord"); LstUsers.Add (joueur3);
+			User joueur4 = new User ("Guigui", "F.", "Guillaume-le-Grand");                     LstUsers.Add (joueur4);
 
 			/***** Initialisation des équipes & gladiateurs *****/
 			/**************************************/
 			List<Team> LstTeams = new List<Team> ();
 			// joueur1
-			Team team1_j1 = new Team(joueur1, 1, "Les Superwariors", "Une team qui mise tout sur la force.", new String[3] {"Marcel", "Henri", "Félix"});                     LstTeams.Add(team1_j1);
+			Team team1_j1 = new Team (joueur1, 1, "Les Superwariors", "Une team qui mise tout sur la force.", new String[3] { "Marcel", "Henri", "Félix" });                     LstTeams.Add (team1_j1);
 			// joueur2
-			Team team1_j2 = new Team(joueur2, 1, "Killers", "La team des killers !", new String[3] {"Hervé", "Steve", "Mamadou"});                                            LstTeams.Add(team1_j2);
+			Team team1_j2 = new Team (joueur2, 1, "Killers", "La team des killers !", new String[3] { "Hervé", "Steve", "Mamadou" });                                            LstTeams.Add (team1_j2);
 			// joueur3
-			Team team1_j3 = new Team(joueur3, 1, "Nyancats", "Les ch'arc-en-ciel en force !", new String[3] {"Jonas", "John", "Jackie"});                                     LstTeams.Add(team1_j3);
+			Team team1_j3 = new Team (joueur3, 1, "Nyancats", "Les ch'arc-en-ciel en force !", new String[3] { "Jonas", "John", "Jackie" });                                     LstTeams.Add (team1_j3);
 			// joueur4
-			Team team1_j4 = new Team(joueur4, 1, "Ligue des justiciers", "Les superjusticiers sont de retour.", new String[3] {"Jean-Pierre", "Jean-René", "Jean-François"}); LstTeams.Add(team1_j4);
+			Team team1_j4 = new Team (joueur4, 1, "Ligue des justiciers", "Les superjusticiers sont de retour.", new String[3] { "Jean-Pierre", "Jean-René", "Jean-François" }); LstTeams.Add (team1_j4);
 
 
 			/***** Affichage des infos (joueurs/équipes/gladiateurs) *****/
 			foreach (User b_joueur in LstUsers) {
 				Console.WriteLine ("- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -");
 
-				Console.WriteLine(	b_joueur.getThis()	);
+				Console.WriteLine (b_joueur.getThis ());
 
 				foreach (Team b_team in LstTeams) {
-					if(b_team.userAlias.Alias == b_joueur.Alias)
-						Console.WriteLine(	b_team.getThis()	);
+					if (b_team.userAlias.Alias == b_joueur.Alias)
+						Console.WriteLine (b_team.getThis ());
 
 					foreach (Gladiator b_glad in b_team.getGladiators())
 						Console.WriteLine (b_glad);
@@ -49,9 +51,10 @@ namespace Gladiateur
 
 
 			/*
-			 * List<Team> sortByTheTwoStrongestTeam = (from b_team in Team
-			       orderby b_team.getPercentVictory() descending
-			       select b_team).Take(2).ToList();
-			*/
+		 * List<Team> sortByTheTwoStrongestTeam = (from b_team in Team
+		       orderby b_team.getPercentVictory() descending
+		       select b_team).Take(2).ToList();
+		*/
 		}
+	}
 }
